0460ad1c937e8328b0a7dab5763c96a5db456123
Naikx <erik.jutzi@googlemail.com>
2020-07-06 22:46:04 +0200

    Updates

---
+++ precache-manifest.2dc89a2b064d96ffbe8bd51476422259.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "037d9546dfff805c3f19933798461265",
+    "url": "/expense-tracker/index.html"
+  },
+  {
+    "revision": "abe8b92ce72f5a9a4969",
+    "url": "/expense-tracker/static/css/main.d0e32a8e.chunk.css"
+  },
+  {
+    "revision": "64629b05b432c44e1bd6",
+    "url": "/expense-tracker/static/js/2.4599387a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense-tracker/static/js/2.4599387a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abe8b92ce72f5a9a4969",
+    "url": "/expense-tracker/static/js/main.14537c4a.chunk.js"
+  },
+  {
+    "revision": "7a228fdbfe8715a85308",
+    "url": "/expense-tracker/static/js/runtime-main.91d22b83.js"
+  }
+]);
